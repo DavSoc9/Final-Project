b134c3953a625ff67286a279c246ad10d1470ff8
DavSoc9 <63879660+DavSoc9@users.noreply.github.com>
2020-05-17 10:38:48 -0700

    Create BookSorter.java

---
+++ BookSorter.java
@@ -0,0 +1,23 @@
+/**
+ *  Final Project
+ *  This abstract class allows us to see books
+ *  CS108
+ *  May 14, 2020
+ *  @author Jesus Davila
+ */
+import java.util.ArrayList;
+
+public abstract class BookSorter implements BookFinder {
+    // This for statement finds the number of books there is
+    public void sort(ArrayList<String> arr){
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = arr.size() - 1; j > i; j--) {
+                if ( arr.get(i).compareTo(arr.get(j)) > 0 ) {
+                    String tmp = arr.get(i);
+                    arr.set(i,arr.get(j));
+                    arr.set(j,tmp);
+                }
+            }
+        }
+    }
+}
